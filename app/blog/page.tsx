58f11981b753239ca3e0d5e651419e75906887a5
liwei <wli@pvetec.com>
2024-05-09 11:39:18 +0800

    delete: blog/page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,8 +4,6 @@ import AddPost from "@/components/post/AddPost";
 import {useGetPostsQuery} from "@/feature/api/postApi";
 
 export default function BlogPage() {
-    const {data: posts} = useGetPostsQuery({current: 0, pageSize: 20})
-    if (posts === undefined) return null
     return (
         <div>
             <AddPost/>
